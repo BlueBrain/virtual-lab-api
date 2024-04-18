2e22a0a9dc8fed7b15d23ea357c1ad69d76d0e43
Dinika Saxena <dinikasaxenas@gmail.com>
2024-04-18 16:38:26 +0200

    Add test for delete user role

--- virtual_labs/tests/test_delete_user_from_lab.py
+++ virtual_labs/tests/test_delete_user_from_lab.py
@@ -27,7 +27,7 @@ def get_invite_token_from_email(recipient_email: str) -> str:
 @pytest_asyncio.fixture
 async def mock_lab_invite(
     async_test_client: AsyncClient,
-) -> AsyncGenerator[tuple[AsyncClient, str, str, str], None]:
+) -> AsyncGenerator[tuple[AsyncClient, str, str], None]:
     # Create lab
     lab_response = await create_mock_lab(async_test_client)
     lab_id = lab_response.json()["data"]["virtual_lab"]["id"]
@@ -56,7 +56,7 @@ async def mock_lab_invite(
         )
     ).id
 
-    yield async_test_client, lab_id, invitee_id, invitee_username
+    yield async_test_client, lab_id, invitee_id
 
     response = await async_test_client.delete(
         f"/virtual-labs/{lab_id}", headers=headers
@@ -68,28 +68,24 @@ def assert_right_users_in_lab(response: Response) -> None:
     assert response.status_code == 200
     lab_users_response_data = response.json()
     lab_users = lab_users_response_data["data"]["users"]
-    assert len(lab_users) == 2
+    assert len(lab_users) == 1
     for user in lab_users:
-        if user["username"] == "test":
-            assert user["role"] == "admin"
-            assert user["invite_accepted"] is True
-        else:
-            assert user["username"] == "test-2"
-            assert user["role"] == "member"
-            assert user["invite_accepted"] is True
+        assert user["username"] == "test"
+        assert user["role"] == "admin"
+        assert user["invite_accepted"] is True
 
 
 @pytest.mark.asyncio
-async def test_change_user_role_in_lab(
-    mock_lab_invite: tuple[AsyncClient, str, str, str],
+async def test_delete_user_from_lab(
+    mock_lab_invite: tuple[AsyncClient, str, str],
 ) -> None:
-    client, lab_id, invitee_id, invitee_username = mock_lab_invite
-    response = await client.patch(
-        f"virtual-labs/{lab_id}/users/{invitee_id}?new_role=member",
-        headers=get_headers(),
+    client, lab_id, invitee_id = mock_lab_invite
+    response = await client.delete(
+        f"virtual-labs/{lab_id}/users/{invitee_id}",
+        headers=get_headers("test"),
     )
     assert response.status_code == 200
     lab_users_response = await client.get(
-        f"/virtual-labs/{lab_id}/users", headers=get_headers(invitee_username)
+        f"/virtual-labs/{lab_id}/users", headers=get_headers("test")
     )
     assert_right_users_in_lab(lab_users_response)
